cc8cd6f937a8921ba13ddf87630b2cc6292462bc
Yuichiro Kaneko <spiketeika@gmail.com>
2018-07-19 21:41:08 +0900

    Add test cases for `State.AddEnv`
    Signed-off-by: Yuichiro Kaneko <spiketeika@gmail.com>

--- client/llb/state_test.go
+++ client/llb/state_test.go
@@ -28,3 +28,21 @@ func TestStateMeta(t *testing.T) {
 	assert.True(t, ok)
 	assert.Equal(t, "abc", v)
 }
+
+func TestAddEnv(t *testing.T) {
+	s := NewState(nil)
+	s = s.AddEnv("key1", "val1")
+	s = s.AddEnv("key2", "val2")
+	s = s.AddEnv("key1", "value1")
+
+	v, ok := s.GetEnv("key1")
+	assert.True(t, ok)
+	assert.Equal(t, "value1", v)
+
+	v, ok = s.GetEnv("key2")
+	assert.True(t, ok)
+	assert.Equal(t, "val2", v)
+
+	_, ok = s.GetEnv("key3")
+	assert.False(t, ok)
+}
